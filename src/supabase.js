6f5483ed08910332a649fb6f3ef1c1cc7204aa4f
Reza Radfar <reza.radfar1306@gmail.com>
2025-12-07 01:01:25 +0330

    adds variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY,
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
